dd0f20e6317f97100237b6346f897fbc47b201b6
Rahul Mahat <aadhyashreemahat@gmail.com>
2024-09-08 14:21:53 +0545

    First time push

---
+++ Load_Dataset.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import torch
+import random
+from scipy.ndimage.interpolation import zoom
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+from torchvision.transforms import functional as F
+from typing import Callable
+import os
+import cv2
+from scipy import ndimage
+from transformers import BertTokenizer,BertModel
+from typing import Optional, List, Dict
+
+def random_rot_flip(image, label):
+    k = np.random.randint(0, 4)
+    image = np.rot90(image, k)
+    label = np.rot90(label, k)
+    axis = np.random.randint(0, 2)
+    image = np.flip(image, axis=axis).copy()
+    label = np.flip(label, axis=axis).copy()
+    return image, label
+
+
+def random_rotate(image, label):
+    angle = np.random.randint(-20, 20)
+    image = ndimage.rotate(image, angle, order=0, reshape=False)
+    label = ndimage.rotate(label, angle, order=0, reshape=False)
+    return image, label
+
+class BertEmbedding:
+    def __init__(self, model_name='bert-base-uncased'):
+        # Load pre-trained BERT model and tokenizer
+        self.tokenizer = BertTokenizer.from_pretrained(model_name)
+        self.model = BertModel.from_pretrained(model_name)
+        self.model.eval()  # Set model to evaluation mode
+
+    def oov(self, batches, oov_way='avg'):
+        """
+        Handle OOV tokens and filter out [CLS], [SEP] tokens.
+        
+        Parameters
+        ----------
+        batches : List[(tokens_id, sequence_outputs)]
+            token_ids: Tensor of shape (max_seq_length,)
+            sequence_outputs: Tensor of shape (max_seq_length, hidden_dim)
+            
+        oov_way : str
+            Use **avg**, **sum** or **last** to get token embedding for OOV words.
+
+        Returns
+        -------
+        List[(List[str], List[torch.Tensor])]
+            List of tokens and corresponding embeddings
+        """
+        sentences = []
+        for token_ids, sequence_outputs in batches:
+            tokens = []
+            tensors = []
+            oov_len = 1
+
+            for token_id, sequence_output in zip(token_ids, sequence_outputs):
+                token = self.tokenizer.convert_ids_to_tokens(token_id.item())
+
+                # Skip [CLS], [SEP], and [PAD] tokens
+                if token in ['[CLS]', '[SEP]', '[PAD]']:
+                    continue
+
+                if token.startswith('##'):
+                    # Subword token, part of an OOV word
+                    token = token[2:]
+                    tokens[-1] += token
+                    if oov_way == 'last':
+                        tensors[-1] = sequence_output
+                    else:
+                        tensors[-1] += sequence_output
+                    if oov_way == 'avg':
+                        oov_len += 1
+                else:  # Handle in-vocabulary tokens
+                    if oov_len > 1:
+                        tensors[-1] /= oov_len
+                        oov_len = 1
+                    tokens.append(token)
+                    tensors.append(sequence_output)
+
+            # Final check if the last token was an OOV word
+            if oov_len > 1:
+                tensors[-1] /= oov_len
+
+            sentences.append((tokens, tensors))
+        
+        return sentences
+
+    def __call__(self, sentences, oov_way='avg'):
+        """
+        Tokenize input sentences, get embeddings, and process OOV words.
+
+        Parameters
+        ----------
+        sentences : List[str]
+            List of input sentences to process.
+        oov_way : str
+            Method to handle OOV tokens: 'avg', 'sum', or 'last'.
+
+        Returns
+        -------
+        List[(List[str], List[torch.Tensor])]
+            Processed tokens and corresponding embeddings.
+        """
+        # Tokenize sentences and get input tensors
+        encoding = self.tokenizer(sentences, return_tensors='pt', padding=True, truncation=True)
+        input_ids = encoding['input_ids']
+        attention_mask = encoding['attention_mask']
+
+        # Get BERT embeddings (sequence outputs)
+        with torch.no_grad():
+            outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+            sequence_outputs = outputs.last_hidden_state  # shape: (batch_size, seq_len, hidden_dim)
+
+        # Prepare batches of (token_ids, sequence_outputs)
+        batches = [(input_ids[i], sequence_outputs[i]) for i in range(input_ids.size(0))]
+
+        # Process OOV words and filter out special tokens
+        return self.oov(batches, oov_way=oov_way)
+
+
+
+class RandomGenerator(object):
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label, text = sample['image'], sample['label'], sample['text']
+        image, label = image.astype(np.uint8), label.astype(np.uint8)
+        image, label = F.to_pil_image(image), F.to_pil_image(label)
+        x, y = image.size
+        if random.random() > 0.5:
+            image, label = random_rot_flip(image, label)
+        elif random.random() > 0.5:
+            image, label = random_rotate(image, label)
+
+        if x != self.output_size[0] or y != self.output_size[1]:
+            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
+            label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+        image = F.to_tensor(image)
+        label = to_long_tensor(label)
+        text = torch.Tensor(text)
+        sample = {'image': image, 'label': label, 'text': text}
+        return sample
+
+
+class ValGenerator(object):
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label, text = sample['image'], sample['label'], sample['text']
+        image, label = image.astype(np.uint8), label.astype(np.uint8)  # OSIC
+        image, label = F.to_pil_image(image), F.to_pil_image(label)
+        x, y = image.size
+        if x != self.output_size[0] or y != self.output_size[1]:
+            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
+            label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+        image = F.to_tensor(image)
+        label = to_long_tensor(label)
+        text = torch.Tensor(text)
+        sample = {'image': image, 'label': label, 'text': text}
+        return sample
+
+
+def to_long_tensor(pic):
+    # handle numpy array
+    img = torch.from_numpy(np.array(pic, np.uint8))
+    # backward compatibility
+    return img.long()
+
+
+def correct_dims(*images):
+    corr_images = []
+    for img in images:
+        if len(img.shape) == 2:
+            corr_images.append(np.expand_dims(img, axis=2))
+        else:
+            corr_images.append(img)
+
+    if len(corr_images) == 1:
+        return corr_images[0]
+    else:
+        return corr_images
+
+
+class LV2D(Dataset):
+    def __init__(self, dataset_path: str, task_name: str, row_text: str, joint_transform: Callable = None,
+                 one_hot_mask: int = False,
+                 image_size: int = 224) -> None:
+        self.dataset_path = dataset_path
+        self.image_size = image_size
+        self.output_path = os.path.join(dataset_path)
+        self.mask_list = os.listdir(self.output_path)
+        self.one_hot_mask = one_hot_mask
+        self.rowtext = row_text
+        self.task_name = task_name
+        self.bert_embedding = BertEmbedding()
+
+        if joint_transform:
+            self.joint_transform = joint_transform
+        else:
+            to_tensor = T.ToTensor()
+            self.joint_transform = lambda x, y: (to_tensor(x), to_tensor(y))
+
+    def __len__(self):
+        return len(os.listdir(self.output_path))
+
+    def __getitem__(self, idx):
+
+        mask_filename = self.mask_list[idx]  # Co
+        mask = cv2.imread(os.path.join(self.output_path, mask_filename), 0)
+        mask = cv2.resize(mask, (self.image_size, self.image_size))
+        mask[mask <= 0] = 0
+        mask[mask > 0] = 1
+        mask = correct_dims(mask)
+        text = self.rowtext[mask_filename]
+        text = text.split('\n')
+        text_token = self.bert_embedding(text)
+        text = np.array(text_token[0][1])
+        if text.shape[0] > 14:
+            text = text[:14, :]
+        if self.one_hot_mask:
+            assert self.one_hot_mask > 0, 'one_hot_mask must be nonnegative'
+            mask = torch.zeros((self.one_hot_mask, mask.shape[1], mask.shape[2])).scatter_(0, mask.long(), 1)
+
+        sample = {'label': mask, 'text': text}
+
+        return sample, mask_filename
+
+
+class ImageToImage2D(Dataset):
+
+    def __init__(self, dataset_path: str, task_name: str, row_text: str, joint_transform: Callable = None,
+                 one_hot_mask: int = False,
+                 image_size: int = 224) -> None:
+        self.dataset_path = dataset_path
+        self.image_size = image_size
+        self.input_path = os.path.join(dataset_path, 'img')
+        self.output_path = os.path.join(dataset_path, 'labelcol')
+        self.images_list = os.listdir(self.input_path)
+        self.mask_list = os.listdir(self.output_path)
+        self.one_hot_mask = one_hot_mask
+        self.rowtext = row_text
+        self.task_name = task_name
+        self.bert_embedding = BertEmbedding()
+
+        if joint_transform:
+            self.joint_transform = joint_transform
+        else:
+            to_tensor = T.ToTensor()
+            self.joint_transform = lambda x, y: (to_tensor(x), to_tensor(y))
+
+    def __len__(self):
+        return len(os.listdir(self.input_path))
+
+    def __getitem__(self, idx):
+
+        image_filename = self.images_list[idx]  # MoNuSeg
+        mask_filename = image_filename[: -3] + "png"  # MoNuSeg
+        # mask_filename = self.mask_list[idx]  # Covid19
+        # image_filename = mask_filename.replace('mask_', '')  # Covid19
+        image = cv2.imread(os.path.join(self.input_path, image_filename))
+        image = cv2.resize(image, (self.image_size, self.image_size))
+
+        # read mask image
+        mask = cv2.imread(os.path.join(self.output_path, mask_filename), 0)
+        mask = cv2.resize(mask, (self.image_size, self.image_size))
+        mask[mask <= 0] = 0
+        mask[mask > 0] = 1
+
+        # correct dimensions if needed
+        image, mask = correct_dims(image, mask)
+        text = self.rowtext[mask_filename]
+        text = text.split('\n')
+        text_token = self.bert_embedding(text)
+        text = np.array(text_token[0][1])
+        if text.shape[0] > 10:
+            text = text[:10, :]
+
+        if self.one_hot_mask:
+            assert self.one_hot_mask > 0, 'one_hot_mask must be nonnegative'
+            mask = torch.zeros((self.one_hot_mask, mask.shape[1], mask.shape[2])).scatter_(0, mask.long(), 1)
+
+        sample = {'image': image, 'label': mask, 'text': text}
+
+        if self.joint_transform:
+            sample = self.joint_transform(sample)
+
+        return sample, image_filename
